fe26fb65ac2c2d6dcf4ee1913ece14f059d94f42
YeChenglin <993912136@qq.com>
2015-12-27 15:56:38 +0800

    kaohe

--- delete.php
+++
@@ -1,8 +0,0 @@
-<meta charset="UTF-8">
-<?php
-	include 'conn.php';
-	$chosen=$_POST['chosen'];
-	$sql="DELETE FROM web_list WHERE descripsion = "."'".$chosen."'";
-	$dbh->query($sql);
-	echo"<script>alert('网址删除成功！！');location.href='".$_SERVER["HTTP_REFERER"]."'</script>";
-?>
